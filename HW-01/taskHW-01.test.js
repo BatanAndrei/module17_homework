34ab2b71623fd135a77e165e4708816c00d9f26a
Batan Andrei <yabatan777@gmail.com>
2023-03-15 15:44:50 +0300

    initial commit

--- HW-01/taskHW-01.test.js
+++ HW-01/taskHW-01.test.js
@@ -1,12 +1,12 @@
 import {getPercents} from './taskHW-01';
 
-describe('', () => {
+describe('checking the getPercents function for a percentage of the number', () => {
 
-  it('', () => {
+  it('add 30 and 200 expect 60', () => {
     expect(getPercents(30, 200)).toBe(60);
     });
 
-    it('', () => {
+    it('add 30 and 200 expect 80', () => {
       expect(getPercents(30, 200)).toBe(80);
       });
 });
