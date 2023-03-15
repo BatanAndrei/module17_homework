230e2a34878b2751e8992c6ce6cb3e2d36a2bfc1
Batan Andrei <yabatan777@gmail.com>
2023-03-15 15:08:26 +0300

    initial commit

--- src/taskHW-01.js
+++ src/taskHW-01.js
@@ -1,8 +1,3 @@
-// export function sum(a, b) {
-//     return a + b;
-//   }
-  //module.exports = sum;
-
   export function reversString(str) {
     let result = str.split('').reverse().join('');
     return result
---
+++ src/taskHW-01.test.js
@@ -0,0 +1,8 @@
+import {reversString} from './taskHW-01';
+
+describe('add -строка get a reverse -акортс', () => {
+
+  it('add -строка get a reverse -акортс', () => {
+    expect(reversString('строка')).toBe('акортс');
+    });
+});
